b4a82cb9952ebd35761d56d2a2390f1e14d95c50
Joshua Tjung <josht1@ad.unc.edu>
2024-11-11 11:56:13 -0500

    test

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('<h1>Calanova<h1>');
 })
 
-app.listen(8001, () => {
-    console.log('port');
+app.listen(8000, () => {
+    console.log('port listening');
 });
