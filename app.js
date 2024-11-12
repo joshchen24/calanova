b430c671c45312933ea3aef3d772bad80673c510
Joshua Tjung <josht1@ad.unc.edu>
2024-11-11 22:20:19 -0500

    ios simulator working

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={() => console.log("text pressed")}>
+        HelloWorld!
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 2,
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
